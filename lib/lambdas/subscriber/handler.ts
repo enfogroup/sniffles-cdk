30da0b522717b59913375acd599fe071ca28d138
Urban Pettersson <urbanpe88@gmail.com>
2022-04-06 20:32:23 +0200

    chore: add parsing of env variables

--- lib/lambdas/subscriber/handler.ts
+++ lib/lambdas/subscriber/handler.ts
@@ -1,16 +1,42 @@
 import * as CloudWatchLogs from 'aws-sdk/clients/cloudwatchlogs'
 import { SSMCache } from '@enfo/aws-secrets'
+import { parseEnvString, VariableType } from '@enfo/env-vars'
+import { parseVariables } from '@enfo/env-vars/clients/parse'
 
 const logsClient = new CloudWatchLogs()
 const ssmClient = new SSMCache({
   defaultTTL: 60,
-  region: process.env.AWS_REGION! // FIXME
+  region: parseEnvString('AWS_REGION', 'eu-west-1')
+})
+
+const env = parseVariables<{
+  kinesisArn: string,
+  logGroupsParameter: string,
+  cloudWatchRole: string
+}>({
+  variables: [
+    {
+      type: VariableType.STRING,
+      name: 'kinesisArn',
+      required: true
+    },
+    {
+      type: VariableType.STRING,
+      name: 'logGroupsParameter',
+      required: true
+    },
+    {
+      type: VariableType.STRING,
+      name: 'cloudWatchRole',
+      required: true
+    }
+  ]
 })
 
 export const handler = async (): Promise<void> => {
   const [groups, patterns] = await Promise.all([
     getLogGroups(),
-    ssmClient.getParameter({ Name: '' })
+    ssmClient.getParameter({ Name: env.logGroupsParameter })
   ])
 }
 
