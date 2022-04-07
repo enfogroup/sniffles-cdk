89381f271164ac3c7770ece337d67e436852c3ca
Urban Pettersson <urbanpe88@gmail.com>
2022-04-07 11:22:34 +0200

    refactor: apply policy statements directly lambda

--- lib/sniffles.ts
+++ lib/sniffles.ts
@@ -6,7 +6,7 @@ import { Construct } from 'constructs'
 import { Stream } from 'aws-cdk-lib/aws-kinesis'
 import { Duration } from 'aws-cdk-lib'
 import { StringListParameter } from 'aws-cdk-lib/aws-ssm'
-import { PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
+import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
 import { RetentionDays } from 'aws-cdk-lib/aws-logs'
 
 export interface SnifflesProps {
@@ -63,45 +63,7 @@ export class Sniffles extends Construct {
   }
 
   private setupSubscriberLambda (props: SetupSubscriberLambdaProps): NodejsFunction {
-    const passRoleStatement = new PolicyStatement({
-      actions: [
-        'iam:PassRole'
-      ],
-      resources: [
-        props.cloudWatchRole
-      ]
-    })
-    const cloudWatchStatement = new PolicyStatement({
-      actions: [
-        'logs:DescribeLogGroups',
-        'logs:DescribeSubscriptionFilters',
-        'logs:PutSubscriptionFilter'
-      ],
-      resources: [
-        '*' // FIXME, narrow down
-      ]
-    })
-    const ssmStatement = new PolicyStatement({
-      actions: [
-        'ssm:GetParameter'
-      ],
-      resources: [
-        props.patternsArn
-      ]
-    })
-    const role = new Role(this, 'SubscriberLambdaRole', {
-      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-      inlinePolicies: {
-        Permissions: new PolicyDocument({
-          statements: [
-            passRoleStatement,
-            cloudWatchStatement,
-            ssmStatement
-          ]
-        })
-      }
-    })
-    return new NodejsFunction(this, 'SubscriberLambda', {
+    const lambda = new NodejsFunction(this, 'SubscriberLambda', {
       entry: join(__dirname, 'lambdas/subscriber/handler.ts'),
       handler: 'handler',
       memorySize: 128,
@@ -114,8 +76,34 @@ export class Sniffles extends Construct {
       },
       environment: {
         ...props
-      },
-      role
+      }
     })
+    lambda.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'iam:PassRole'
+      ],
+      resources: [
+        props.cloudWatchRole
+      ]
+    }))
+    lambda.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'logs:DescribeLogGroups',
+        'logs:DescribeSubscriptionFilters',
+        'logs:PutSubscriptionFilter'
+      ],
+      resources: [
+        '*' // FIXME, narrow down
+      ]
+    }))
+    lambda.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameter'
+      ],
+      resources: [
+        props.patternsArn
+      ]
+    }))
+    return lambda
   }
 }
