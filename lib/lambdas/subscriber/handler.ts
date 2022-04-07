4da35da3a15d4087b4406f7bfc84194895668cec
Urban Pettersson <urbanpe88@gmail.com>
2022-04-07 10:43:04 +0200

    chore: add logic for subscriber lambda

--- lib/lambdas/subscriber/handler.ts
+++ lib/lambdas/subscriber/handler.ts
@@ -1,56 +1,100 @@
 import * as CloudWatchLogs from 'aws-sdk/clients/cloudwatchlogs'
 import { SSMCache } from '@enfo/aws-secrets'
-import { parseEnvString, VariableType } from '@enfo/env-vars'
-import { parseVariables } from '@enfo/env-vars/clients/parse'
+import { parseEnvString, parseVariables, VariableType } from '@enfo/env-vars'
 
-const logsClient = new CloudWatchLogs()
-const ssmClient = new SSMCache({
-  defaultTTL: 60,
-  region: parseEnvString('AWS_REGION', 'eu-west-1')
+import tap from 'ramda/src/tap'
+import split from 'ramda/src/split'
+import map from 'ramda/src/map'
+import trim from 'ramda/src/trim'
+import pipe from 'ramda/src/pipe'
+import concat from 'ramda/src/concat'
+import anyPass from 'ramda/src/anyPass'
+import filter from 'ramda/src/filter'
+import reject from 'ramda/src/reject'
+
+const ssmCache = new SSMCache({
+  region: parseEnvString('AWS_REGION', 'eu-west-1'),
+  defaultTTL: 60
 })
-
-const env = parseVariables<{
+const cwl = new CloudWatchLogs()
+const { kinesisArn, patternsName, cloudWatchRole } = parseVariables<{
   kinesisArn: string,
-  logGroupsParameter: string,
+  patternsName: string,
   cloudWatchRole: string
 }>({
   variables: [
     {
-      type: VariableType.STRING,
       name: 'kinesisArn',
+      type: VariableType.STRING,
       required: true
     },
     {
+      name: 'pattersName',
       type: VariableType.STRING,
-      name: 'logGroupsParameter',
       required: true
     },
     {
-      type: VariableType.STRING,
       name: 'cloudWatchRole',
+      type: VariableType.STRING,
       required: true
     }
   ]
 })
 
-export const handler = async (): Promise<void> => {
-  const [groups, patterns] = await Promise.all([
-    getLogGroups(),
-    ssmClient.getParameter({ Name: env.logGroupsParameter })
+const getPatterns = (): Promise<string[]> =>
+  ssmCache.getParameter({ Name: patternsName })
+    .then(split(','))
+    .then(map(trim))
+    .then(tap(console.log))
+const toRegExp = (str: string) => new RegExp(str)
+const test = (re: RegExp) => (str: string) => re.test(str)
+const patternsToFunctions = map(
+  pipe(
+    toRegExp,
+    test
+  )
+)
+const getLogGroups = (token?: string): Promise<CloudWatchLogs.LogGroup[]> =>
+  cwl.describeLogGroups({ nextToken: token }).promise()
+    .then(({ logGroups, nextToken }) =>
+      !nextToken
+        ? logGroups ?? []
+        : getLogGroups(nextToken).then(concat(logGroups ?? [])))
+const getLogGroupsAndPatterns = (): Promise<[string[], string[]]> =>
+  Promise.all([
+    getLogGroups()
+      .then((logGroups: CloudWatchLogs.LogGroup[]): string[] => {
+        return logGroups.reduce((acc: string[], curr: CloudWatchLogs.LogGroup): string[] => {
+          if (!curr.logGroupName) {
+            return acc
+          }
+          acc.push(curr.logGroupName)
+          return acc
+        }, [])
+      }),
+    getPatterns()
   ])
-}
+const filterLogGroups = ([logGroupNames, patterns]: [string[], string[]]) =>
+  pipe(
+    reject(test(/Sniffles/)), // if anything in the Sniffle pipeline gets into the pipeline we'll get a feedback loop
+    filter(anyPass(patternsToFunctions(patterns)))
+  )(logGroupNames)
+const subscribeLogGroup = (logGroupName: string) =>
+  cwl.putSubscriptionFilter({
+    logGroupName,
+    roleArn: cloudWatchRole,
+    filterPattern: '',
+    filterName: 'LogsToKinesis',
+    destinationArn: kinesisArn,
+    distribution: 'Random'
+  }).promise()
+    .catch(console.warn)
 
-const getLogGroups = async (): Promise<string[]> => {
-  const groups: string[] = []
-  let nextToken
-  let logGroups: CloudWatchLogs.DescribeLogGroupsResponse['logGroups']
-  while (true) {
-    ({ logGroups, nextToken } = await logsClient.describeLogGroups({ nextToken }).promise())
-    if (logGroups) {
-      groups.push(...logGroups.map((group) => group.logGroupName || ''))
-    }
-    if (!nextToken) {
-      return groups
-    }
-  }
-}
+exports.handler = (): Promise<string | void> =>
+  getLogGroupsAndPatterns()
+    .then(filterLogGroups)
+    .then(tap(console.log))
+    .then(map(subscribeLogGroup))
+    .then(Promise.all.bind(Promise))
+    .then(() => 'OK')
+    .catch(console.error)
