c6f4c0113dbf79adf9207ee6f80142388120cb75
Urban Pettersson <urbanpe88@gmail.com>
2022-04-13 10:27:39 +0200

    test: add tests for opsgenie lambda

--- lib/opsGenieLambda.ts
+++ lib/opsGenieLambda.ts
@@ -13,18 +13,23 @@ interface LogLine {
 const sns = new SNS()
 const topic = parseEnvString('topic', { required: true })
 
+// istanbul ignore next
+export const publish = async (input: SNS.PublishInput): Promise<void> => {
+  await sns.publish(input).promise()
+}
+
 export const handler = async (event: SNSEvent): Promise<string> => {
   const logMessage: LogMessage = JSON.parse(event.Records[0].Sns.Message)
   const { logEvents, owner, logGroup } = logMessage
   const logLine: LogLine = JSON.parse(logEvents[0].message)
 
-  await sns.publish({
+  await publish({
     TopicArn: topic,
     Subject: take(100, `${owner} ${logGroup} ${logLine.message}`),
     Message: JSON.stringify(logMessage),
     MessageAttributes: {
       eventType: { DataType: 'String', StringValue: 'create' }
     }
-  }).promise()
+  })
   return 'OK'
 }
---
+++ test/opsgenie.spec.ts
@@ -0,0 +1,45 @@
+import * as opsGenie from '../lib/opsGenieLambda'
+
+import { SNSEvent } from 'aws-lambda'
+import { LogMessage } from '../lib/filterLambda'
+
+describe('OpsGenie lambda', () => {
+  describe('handler', () => {
+    it('should extract and format input for sns publish', async () => {
+      const logLine = {
+        message: 'hello there'
+      }
+      const logMessage: LogMessage = {
+        owner: '111122223333',
+        logEvents: [{
+          id: '123',
+          timestamp: 1,
+          message: JSON.stringify(logLine)
+        }],
+        logGroup: 'logGroup!'
+      } as unknown as LogMessage
+      const input: SNSEvent = {
+        Records: [{
+          Sns: {
+            Message: JSON.stringify(logMessage)
+          }
+        }]
+      } as unknown as SNSEvent
+      const publishMock = jest.spyOn(opsGenie, 'publish').mockResolvedValue()
+
+      const output = await opsGenie.handler(input)
+
+      expect(output).toEqual('OK')
+      expect(publishMock.mock.calls[0][0]).toMatchObject({
+        Message: '{"owner":"111122223333","logEvents":[{"id":"123","timestamp":1,"message":"{\\"message\\":\\"hello there\\"}"}],"logGroup":"logGroup!"}',
+        Subject: '111122223333 logGroup! hello there',
+        MessageAttributes: {
+          eventType: {
+            DataType: 'String',
+            StringValue: 'create'
+          }
+        }
+      })
+    })
+  })
+})
