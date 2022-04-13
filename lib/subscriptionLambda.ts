62af62e9f8868d306bd944694d24650cf8b722c7
Urban Pettersson <urbanpe88@gmail.com>
2022-04-13 11:46:00 +0200

    feat: add support for exclusions to subcribeLogs

--- lib/subscriptionLambda.ts
+++ lib/subscriptionLambda.ts
@@ -13,12 +13,13 @@ import reject from 'ramda/src/reject'
 
 const ssmCache = new SSMCache({
   region: parseEnvString('AWS_REGION', 'eu-west-1'),
-  defaultTTL: 60
+  defaultTTL: 300
 })
 const cwl = new CloudWatchLogs()
-const { kinesisStream, inclusions, cloudWatchRole } = parseVariables<{
+const { kinesisStream, inclusions, exclusions, cloudWatchRole } = parseVariables<{
   kinesisStream: string,
   inclusions: string,
+  exclusions: string,
   cloudWatchRole: string
 }>({
   variables: [
@@ -45,10 +46,15 @@ const { kinesisStream, inclusions, cloudWatchRole } = parseVariables<{
   ]
 })
 
-const getPatterns = (): Promise<string[]> =>
+const getInclusionPatterns = (): Promise<string[]> =>
   ssmCache.getStringListParameter({ Name: inclusions })
     .then(map(trim))
     .then(tap(console.log))
+const getExclusionPatterns = (): Promise<string[]> =>
+  ssmCache.getStringListParameter({ Name: exclusions })
+    .then(map(trim))
+    .then(tap(console.log))
+
 const toRegExp = (str: string) => new RegExp(str)
 const test = (re: RegExp) => (str: string) => re.test(str)
 const patternsToFunctions = map(
@@ -63,7 +69,7 @@ const getLogGroups = (token?: string): Promise<CloudWatchLogs.LogGroup[]> =>
       !nextToken
         ? logGroups ?? []
         : getLogGroups(nextToken).then(concat(logGroups ?? [])))
-const getLogGroupsAndPatterns = (): Promise<[string[], string[]]> =>
+const getLogGroupsAndPatterns = (): Promise<[string[], string[], string[]]> =>
   Promise.all([
     getLogGroups()
       .then((logGroups: CloudWatchLogs.LogGroup[]): string[] => {
@@ -75,12 +81,14 @@ const getLogGroupsAndPatterns = (): Promise<[string[], string[]]> =>
           return acc
         }, [])
       }),
-    getPatterns()
+    getInclusionPatterns(),
+    getExclusionPatterns()
   ])
-const filterLogGroups = ([logGroupNames, patterns]: [string[], string[]]) =>
+const filterLogGroups = ([logGroupNames, inclusionPatterns, exclusionPatterns]: [string[], string[], string[]]) =>
   pipe(
     reject(test(/Sniffles/)), // if anything in the Sniffle pipeline gets into the pipeline we'll get a feedback loop
-    filter(anyPass(patternsToFunctions(patterns)))
+    filter(anyPass(patternsToFunctions(inclusionPatterns))),
+    reject(anyPass(patternsToFunctions(exclusionPatterns)))
   )(logGroupNames)
 const subscribeLogGroup = (logGroupName: string) =>
   cwl.putSubscriptionFilter({
