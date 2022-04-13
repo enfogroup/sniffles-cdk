1b6998a785a089f1a9a6883790c166760143276d
Urban Pettersson <urbanpe88@gmail.com>
2022-04-13 20:39:28 +0200

    fix: change stack to construct

--- lib/opsGenie/index.ts
+++ lib/opsGenie/index.ts
@@ -1,4 +1,4 @@
-import { Duration, Stack } from 'aws-cdk-lib'
+import { Duration } from 'aws-cdk-lib'
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { RetentionDays } from 'aws-cdk-lib/aws-logs'
@@ -6,6 +6,7 @@ import { Topic } from 'aws-cdk-lib/aws-sns'
 import { Queue } from 'aws-cdk-lib/aws-sqs'
 import { Construct } from 'constructs'
 import { join } from 'path'
+
 import { setupLambdaAlarms, setupQueueAlarms } from '../alarms'
 
 /**
@@ -26,7 +27,7 @@ export interface OpsGenieForwarderProps {
  * Formats and forwards logs to an SNS Topic
  * Messages will be formatted to work with OpsGenie SNS hooks
  */
-export class OpsGenieForwarder extends Stack {
+export class OpsGenieForwarder extends Construct {
   public lambda: NodejsFunction
   constructor (scope: Construct, id: string, props: OpsGenieForwarderProps) {
     super(scope, id)
