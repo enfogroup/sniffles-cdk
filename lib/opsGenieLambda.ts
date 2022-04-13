a39d30795b41076171e43069d83c71e9dbc96821
Urban Pettersson <urbanpe88@gmail.com>
2022-04-13 07:55:30 +0200

    fix: correct logic in opsGenieLambda

--- lib/opsGenieLambda.ts
+++ lib/opsGenieLambda.ts
@@ -1,21 +1,25 @@
-import { SNSEvent } from 'aws-lambda'
+import { parseEnvString } from '@enfo/env-vars'
 import SNS from 'aws-sdk/clients/sns'
 import take from 'ramda/src/take'
 
 import { LogMessage } from './filterLambda'
+
+import { SNSEvent } from 'aws-lambda'
 
 interface LogLine {
   message: string
 }
 
 const sns = new SNS()
+const topic = parseEnvString('topic', { required: true })
 
 export const handler = async (event: SNSEvent): Promise<string> => {
   const logMessage: LogMessage = JSON.parse(event.Records[0].Sns.Message)
-  const { message, owner, logGroup } = logMessage
-  const logLine: LogLine = JSON.parse(message)
+  const { logEvents, owner, logGroup } = logMessage
+  const logLine: LogLine = JSON.parse(logEvents[0].message)
 
   await sns.publish({
+    TopicArn: topic,
     Subject: take(100, `${owner} ${logGroup} ${logLine.message}`),
     Message: JSON.stringify(logMessage),
     MessageAttributes: {
